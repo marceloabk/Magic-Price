6bb27b94467a94eab30c51fb0d48e8693ddc6bec
Marcelo Cristiano <marceloabk@hotmail.com>
2018-06-10 23:24:14 -0300

    Removing curl from project

--- crawlers/UGCCrawler.js
+++ crawlers/UGCCrawler.js
@@ -1,4 +1,3 @@
-const curl = require('curl')
 const request = require('superagent')
 const Throttle = require('superagent-throttle')
 
@@ -18,48 +17,46 @@ function crawlUGC(i, priceArray) {
   .end((err, res) => {
     const data = res.body
     const result = {name: '', price: Infinity, availability: 0}
-    curl.getJSON(url, null, (err, response, data) => {
-      for (let j = 0; j < data.length; j++) {
-        if (data[j].estoque > 0 && data[j].preco < result.price) {
-          result.name = data[j].nome
-          result.price = Number(data[j].preco)
-          result.availability = data[j].estoque
-        }
+    for (let j = 0; j < data.length; j++) {
+      if (data[j].estoque > 0 && data[j].preco < result.price) {
+        result.name = data[j].nome
+        result.price = Number(data[j].preco)
+        result.availability = data[j].estoque
       }
+    }
 
-      if (result.price !== Infinity) {
-        priceArray.push(result.price * i.quantity)
+    if (result.price !== Infinity) {
+      priceArray.push(result.price * i.quantity)
 
-        const normalizedSelector = normalizeSelector(i.name)
-        const searchName = document.querySelector(`.${normalizedSelector}`)
-        if (searchName === null) {
-          const li = document.createElement('li')
-          const div = document.createElement('div')
-          div.className = `collapsible-header`
-
-          const itemText = document.createTextNode(result.name)
-
-          div.appendChild(itemText)
-          li.appendChild(div)
-          const body = createCollapsibleBody(`UGC: ${result.name}: ${result.price.toFixed(2)} | Quantidade: ${result.availability}`, normalizedSelector)
-          li.appendChild(body)
-          ul.appendChild(li)
-        } else {
-          searchName.innerHTML += `<br />UGC: ${result.name}: ${result.price.toFixed(2)} | Quantidade: ${result.availability}`
-        }
-
-        calculatePrice(priceArray, 'UGC', '.ugc-price')
-      } else {
-        // Imperial Seal
-        const itemText = document.createTextNode(`${i.name}`)
+      const normalizedSelector = normalizeSelector(i.name)
+      const searchName = document.querySelector(`.${normalizedSelector}`)
+      if (searchName === null) {
+        const li = document.createElement('li')
         const div = document.createElement('div')
-        div.className = 'red-text text-lighten-1'
+        div.className = `collapsible-header`
+
+        const itemText = document.createTextNode(result.name)
 
         div.appendChild(itemText)
-        const ugcCards = document.querySelector('.ugc-cards')
-        ugcCards.appendChild(div)
+        li.appendChild(div)
+        const body = createCollapsibleBody(`UGC: ${result.name}: ${result.price.toFixed(2)} | Quantidade: ${result.availability}`, normalizedSelector)
+        li.appendChild(body)
+        ul.appendChild(li)
+      } else {
+        searchName.innerHTML += `<br />UGC: ${result.name}: ${result.price.toFixed(2)} | Quantidade: ${result.availability}`
       }
-    })
+
+      calculatePrice(priceArray, 'UGC', '.ugc-price')
+    } else {
+      // Imperial Seal
+      const itemText = document.createTextNode(`${i.name}`)
+      const div = document.createElement('div')
+      div.className = 'red-text text-lighten-1'
+
+      div.appendChild(itemText)
+      const ugcCards = document.querySelector('.ugc-cards')
+      ugcCards.appendChild(div)
+    }
   })
 }
 
