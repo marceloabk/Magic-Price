6f4aa6191a05035c962c913a66717a7c7d18551b
Marcelo Cristiano <marceloabk@hotmail.com>
2022-10-13 13:03:58 -0300

    wip: refactor

--- src/main/renderer.js
+++ src/main/renderer.js
@@ -10,7 +10,102 @@ let otemplosmPrice = [];
 let clmtgPrice = [];
 
 let allPrices = {};
-let allCards;
+let allCards = new Set();
+
+const bestResultButton = document.getElementById('bestResult')
+bestResultButton.addEventListener('click', () => {
+  const cardNames = [...allCards];
+  console.log({ cardNames })
+  const storeNames = Object.keys(allPrices);
+  console.log({ storeNames })
+  const arrayToBeMultiplied = cardNames.map(() => storeNames);
+  console.log({ arrayToBeMultiplied })
+
+  let resultMatrix = product(arrayToBeMultiplied);
+  console.log({ resultMatrix })
+  resultMatrix.forEach((line, i) => {
+    line.forEach((element, j) => {
+      resultMatrix[i][j] += `|${cardNames[j]}`;
+    });
+  });
+
+  let priceMatrix = JSON.parse(JSON.stringify(resultMatrix));
+  resultMatrix.forEach((line, i) => {
+    line.forEach((element, j) => {
+      const [storeName, cardName] = resultMatrix[i][j].split("|");
+      console.log({ storeName, cardName });
+      priceMatrix[i][j] = allPrices[storeName][cardName];
+    });
+  });
+
+  let bestIndex,
+    total = Infinity;
+  priceMatrix.forEach((priceArray, index) => {
+    const newTotal = priceArray.reduce((a, b) => a + b, 0);
+    if (!isNaN(newTotal) && newTotal < total) {
+      total = newTotal;
+      bestIndex = index;
+    }
+  });
+
+  console.log({ priceMatrix });
+  console.log({ resultMatrix });
+  console.log({ total });
+  console.log({ bestIndex });
+  console.log({
+    result: resultMatrix[bestIndex],
+    total: priceMatrix[bestIndex].reduce((a, b) => a + b, 0),
+  });
+
+  const bestPriceTextNode = document.createTextNode(
+    priceMatrix[bestIndex].reduce((a, b) => a + b, 0)
+  );
+  const bestPriceNode = document.querySelector(".bestResult-price");
+  bestPriceNode.appendChild(bestPriceTextNode);
+
+  resultMatrix[bestIndex].forEach((card) => {
+    const itemText = document.createTextNode(card);
+    const div = document.createElement("div");
+    div.appendChild(itemText);
+
+    const node = document.querySelector(".bestResult-cards");
+    node.appendChild(div);
+  });
+
+})
+
+
+function product(elements) {
+	if (!Array.isArray(elements)) {
+		throw new TypeError();
+	}
+
+	var end = elements.length - 1,
+		result = [];
+
+	function addTo(curr, start) {
+		var first = elements[start],
+			last = (start === end);
+
+		for (var i = 0; i < first.length; ++i) {
+			var copy = curr.slice();
+			copy.push(first[i]);
+
+			if (last) {
+				result.push(copy);
+			} else {
+				addTo(copy, start + 1);
+			}
+		}
+	}
+
+	if (elements.length) {
+		addTo([], 0);
+	} else {
+		result.push([]);
+	}
+	return result;
+}
 
 const createCollapsibleBody = (text, name) => {
   const textNode = document.createTextNode(text);
@@ -99,13 +194,14 @@ window.add.onCards((_event, {item, card, store}) => {
   ul.className = "collapsible";
   const elems = document.querySelectorAll('.collapsible');
   M.Collapsible.init(elems);
+  allCards.add(item.name)
 
 
   if (store === "asgardstore") {
     if (item.price !== Infinity) {
       createCardElement(card.name, store, item, allPrices, ul);
       asgardstorePrice.push(item.price * card.quantity);
-      calculatePrice(asgardstorePrice, store, `.${store}-price`);
+      calculatePrice(asgardstorePrice,`.${store}-price`);
     } else {
       // Imperial Seal
       createCardNotFound(card.name, `.${store}-cards`);
@@ -114,7 +210,7 @@ window.add.onCards((_event, {item, card, store}) => {
     if (item.price !== Infinity) {
       createCardElement(card.name, store, item, allPrices, ul);
       mtgcardsgamesPrice.push(item.price * card.quantity);
-      calculatePrice(mtgcardsgamesPrice, store, `.${store}-price`);
+      calculatePrice(mtgcardsgamesPrice,`.${store}-price`);
     } else {
       // Imperial Seal
       createCardNotFound(card.name, `.${store}-cards`);
@@ -123,7 +219,7 @@ window.add.onCards((_event, {item, card, store}) => {
     if (item.price !== Infinity) {
       createCardElement(card.name, store, item, allPrices, ul);
       spellcastgamesPrice.push(item.price * card.quantity);
-      calculatePrice(spellcastgamesPrice, store, `.${store}-price`);
+      calculatePrice(spellcastgamesPrice,`.${store}-price`);
     } else {
       // Imperial Seal
       createCardNotFound(card.name, `.${store}-cards`);
@@ -132,7 +228,7 @@ window.add.onCards((_event, {item, card, store}) => {
     if (item.price !== Infinity) {
       createCardElement(card.name, store, item, allPrices, ul);
       otemplosmPrice.push(item.price * card.quantity);
-      calculatePrice(otemplosmPrice, store, `.${store}-price`);
+      calculatePrice(otemplosmPrice,`.${store}-price`);
     } else {
       // Imperial Seal
       createCardNotFound(card.name, `.${store}-cards`);
@@ -141,7 +237,7 @@ window.add.onCards((_event, {item, card, store}) => {
     if (item.price !== Infinity) {
       createCardElement(card.name, store, item, allPrices, ul);
       clmtgPrice.push(item.price * card.quantity);
-      calculatePrice(clmtgPrice, store, `.${store}-price`);
+      calculatePrice(clmtgPrice,`.${store}-price`);
     } else {
       // Imperial Seal
       createCardNotFound(card.name, `.${store}-cards`);
